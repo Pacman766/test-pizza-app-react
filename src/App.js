a26841104b1fd346fe1e3fede13cb5ea25ce36e7
pacman766 <3334976@gmail.com>
2022-03-23 10:29:37 +0300

    feat: format work space

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+import './app.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [error, setError] = useState(undefined);
+
+  const fetchGuests = async () => {
+    try {
+      const response = await fetch(
+        'https://gp-js-test.herokuapp.com/pizza/guests'
+      );
+      // console.log(response);
+      const data = await response.json();
+      // console.log(data.party);
+      return data.party;
+    } catch (error) {
+      setError(error);
+      throw error;
+    }
+  };
+
+  const fetchDiets = async (people) => {
+    const encoded = people.map(({ name }) => encodeURIComponent(name));
+
+    try {
+      const response = await fetch(
+        `https://gp-js-test.herokuapp.com/pizza/world-diets-book/${encoded}`
+      );
+
+      const data = await response.json();
+      console.log(data.diet);
+      return data.diet;
+    } catch (error) {
+      setError(error);
+      throw error;
+    }
+  };
+
+  const people = [
+    { name: 'Anton Chehov', isVegan: true },
+    { name: 'Vladimir Pushkin', isVegan: false },
+  ];
+  fetchDiets(people);
+
+  return <div className="App"></div>;
 }
 
 export default App;
